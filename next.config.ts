d0f6542514218b9716352c50f07d7a0ec5e8c004
WIC2025 Developer <developer@wic2025.com>
2025-12-08 20:49:21 +0900

    Update next.config.ts to use unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
